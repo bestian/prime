59b06743a1cdce2f79835442c517a13f132fdaa3
Bestian Tang <bestian@gmail.com>
2021-03-04 23:01:08 +0800

    deploy

---
+++ precache-manifest.6fe056ba4cdce7acbc58a7bc08c3edee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ce89b2967aa4f67e0c",
+    "url": "/prime/css/app.5460949e.css"
+  },
+  {
+    "revision": "f683e151274ba0dd7b0515761c0afb55",
+    "url": "/prime/index.html"
+  },
+  {
+    "revision": "51ce89b2967aa4f67e0c",
+    "url": "/prime/js/app.a42d46a3.js"
+  },
+  {
+    "revision": "e83655b3a4239a4a7f8c",
+    "url": "/prime/js/chunk-vendors.b19685d1.js"
+  },
+  {
+    "revision": "c42ccf95d63660fbcd9d406d8dbbc3c7",
+    "url": "/prime/logo.png"
+  },
+  {
+    "revision": "b36314c992278c67d0309da6e8ae39bf",
+    "url": "/prime/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/prime/robots.txt"
+  }
+]);
